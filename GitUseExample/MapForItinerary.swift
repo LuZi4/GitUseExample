0ef3813dcf5ae30c1958b6353c4fd74c97186e76
Flexus <flexus@fln-1048.local>
2018-12-05 10:21:02 +0100

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Flexus on 05.12.18.
+//  Copyright © 2018 Flexus. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
